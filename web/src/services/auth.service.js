da0074ea4e946d102d33b407f2b80ac6d50f5966
tariq770871-jpg <tariq770871@gmail.com>
2026-07-02 06:05:16 +0300

    feat: add changePassword API call

--- web/src/services/auth.service.js
+++ web/src/services/auth.service.js
@@ -1,12 +1,7 @@
 import api from './api'
-
 export const authApi = {
-  login: (username, password) =>
-    api.post('/auth/login', { username, password }),
-
-  register: (data) =>
-    api.post('/auth/register', data),
-
-  me: () =>
-    api.get('/auth/me'),
+  login: (username, password) => api.post('/auth/login', { username, password }),
+  register: (data) => api.post('/auth/register', data),
+  me: () => api.get('/auth/me'),
+  changePassword: (data) => api.post('/auth/change-password', data),
 }
